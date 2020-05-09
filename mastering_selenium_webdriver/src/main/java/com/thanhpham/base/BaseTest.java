be763d7d1fb8d8cd3607649874a344cbbfc0f684
Thanh <thanhphamnl@gmail.com>
2020-05-09 15:00:23 -0700

    Added alerts, window&Frame, Browsers Test.

--- mastering_selenium_webdriver/src/main/java/com/thanhpham/base/BaseTest.java
+++ mastering_selenium_webdriver/src/main/java/com/thanhpham/base/BaseTest.java
@@ -1,5 +1,11 @@
 package com.thanhpham.base;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,13 +16,12 @@ public class BaseTest {
 
 	protected WebDriver driver;
 
-
 	@BeforeMethod(alwaysRun = true)
 	@Parameters({ "browser" })
 	protected void setUp(@Optional("chrome") String browser) {
 		BrowserDriverFactory factory = new BrowserDriverFactory(browser);
 		driver = factory.createDriver();
-		
+
 		// You don't need to have this sleep.
 		// I added it only for you to see the browser
 		try {
@@ -29,11 +34,31 @@ public class BaseTest {
 		driver.manage().window().maximize();
 	}
 
-
 	@AfterMethod(alwaysRun = true)
 	protected void tearDown() {
 		// Closing driver
 		System.out.println("[Closing driver]");
 		driver.quit();
+	}
+
+
+	// STATIC SLEEP FOR STUDENTS TO SEE TEST EXECUTION
+	protected void sleep() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	protected void takeScreenshot(String fileName) {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "//test-output//screenshots//" + fileName + ".png";
+		try {
+			FileUtils.copyFile(scrFile, new File(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- mastering_selenium_webdriver/src/main/java/com/thanhpham/base/BrowserDriverFactory.java
+++ mastering_selenium_webdriver/src/main/java/com/thanhpham/base/BrowserDriverFactory.java
@@ -5,16 +5,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class BrowserDriverFactory {
 
 	private ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 	private String browser;
 
-
 	public BrowserDriverFactory(String browser) {
 		this.browser = browser.toLowerCase();
 	}
-
 
 	public WebDriver createDriver() {
 		System.out.println("[Setting up driver: " + browser + "]");
@@ -43,6 +52,68 @@ public class BrowserDriverFactory {
 			break;
 		}
 
+		return driver.get();
+	}
+	// to get the URL address, run the command below: 
+	//C:\Users\thanh\git\mastering_selenium_webdriver\mastering_selenium_webdriver\src\main>java
+	// -jar selenium-server-standalone-3.141.59.jar -role hub
+
+	/** Starting tests using Selenium grid */
+	public WebDriver createDriverGrid() {
+		// Setting up selenium grid hub url
+		URL url = null;
+		try {
+			url = new URL("http://192.168.1.70:4444/wd/hub");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Starting " + browser + " on grid");
+
+		// Using DesiredCapabilities to set up browser
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName(browser);
+
+		// Creating driver
+		try {
+			driver.set(new RemoteWebDriver(url, capabilities));
+		} catch (WebDriverException e) {
+			if (e.getMessage().contains("Error forwarding")) {
+				System.out.println("[Couldn't set: " + browser + ". Its unknown. Starting chrome instead]");
+				capabilities.setBrowserName("chrome");
+				driver.set(new RemoteWebDriver(url, capabilities));
+			}
+		}
+
+		return driver.get();
+	}
+
+	/** Starting tests using sauce labs grid */
+	public WebDriver createDriverSauce(String platform, String testName) {
+		System.out.println("[Setting up driver: " + browser + " on SauceLabs]");
+		String username = "masteringselenium";
+		String accessKey = "f52cc90b-6273-4935-a916-c7eb9b0d31f4";
+		String url = "http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub";
+
+		// Setting up DesiredCapabilities (browser and os)
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("browserName", browser);
+
+		if (platform == null) {
+			// If platform is not provided, start tests on Windows 10
+			caps.setCapability("platform", "Windows 10");
+		} else {
+			caps.setCapability("platform", platform);
+		}
+
+		// Setting up test name
+		caps.setCapability("name", testName);
+
+		try {
+			driver.set(new RemoteWebDriver(new URL(url), caps));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 		return driver.get();
 	}
 
